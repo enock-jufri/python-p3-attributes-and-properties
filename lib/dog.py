db506578a86fd9bcceec1f02f76cfa9f72de3aa1
enock-jufri <enockjufri@gmail.com>
2025-01-09 14:15:07 +0300

    properties and attributes

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,42 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None):
+        # Only set and validate name if it's explicitly provided
+        if name is not None:
+            self.name = name  # Uses the setter
+        else:
+            self._name = None
+
+        # Only set and validate breed if it's explicitly provided
+        if breed is not None:
+            self.breed = breed  # Uses the setter
+        else:
+            self._breed = None
+
+    def get_name(self):
+        return getattr(self, "_name", None)  # Safe access with a default value
+
+    def set_name(self, name):
+        if isinstance(name, str) and 0 < len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            self._name = None
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return getattr(self, "_breed", None)  # Safe access with a default value
+
+    def set_breed(self, breed):
+        if breed:
+            if breed.capitalize() in APPROVED_BREEDS:
+                self._breed = breed.capitalize()
+            else:
+                print("Breed must be in list of approved breeds.")
+                self._breed = None
+        else:
+            self._breed = None  # Allow empty or `None` without printing an error
+
+    breed = property(get_breed, set_breed)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,42 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name=None, job=None):
+        self.name = name  # Uses the setter (and name can be None)
+        self.job = job    # Uses the setter
+    
+    # Getter for `name`
+    def get_name(self):
+        return getattr(self, "_name", None)
+    
+    # Setter for `name`
+    def set_name(self, name):
+        if name is not None and (isinstance(name, str) and 0 < len(name) <= 25):
+            self._name = name.title()  # Apply title case
+        elif name is not None:
+            print("Name must be string between 1 and 25 characters.")
+            self._name = None
+        else:
+            self._name = None  # If name is not provided, leave it as None.
+
+    name = property(get_name, set_name)  # Correct property definition
+
+    # Getter for `job`
+    def get_job(self):
+        return getattr(self, "_job", None)
+    
+    # Setter for `job`
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job  # Set job
+        else:
+            print("Job must be in list of approved jobs.")
+            self._job = None
+
+    job = property(get_job, set_job)  # Correct property definition
+
+
+# Example Usage
+person = Person(job="Admin")  # Now `name` can be omitted
+print(person.name)  # Output: None (since no name was provided)
+print(person.job)   # Output: Admin
